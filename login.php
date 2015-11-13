92b52f3c7a4ea1d5b874dcf607e2484d1a5d8d83
Pitones <kai3092@tamu.edu>
2015-11-12 22:37:47 -0600

    changes

--- login.php
+++ login.php
@@ -1,23 +1,28 @@
+<?php ini_set('display_errors', '1'); ?>
+
 <?php 
 
-define('DB_HOST', 'localhost'); 
-define('DB_NAME', 'practice'); 
-define('DB_USER','root'); 
-define('DB_PASSWORD',''); 
+define('DB_HOST', 'database-new.cse.tamu.edu'); 
+define('DB_NAME', 'kai3092-db1'); 
+define('DB_USER','kai3092'); 
+define('DB_PASSWORD','Iamkingoftheworld!'); 
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
 
- /* $ID = $_POST['user']; 
-$Password = $_POST['pass']; 
-*/ 
+
 
 function SignIn() { 
-session_start(); //starting the session for user profile page 
+
+session_start(); //starting the session for user profile page
+
+
 
 	if(!empty($_POST['user'])) //checking the 'user' name which is from Sign-In.html, is it empty or have some text 
 	{ 
+	 
 		$query = mysql_query("SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error()); 
-		$row = mysql_fetch_array($query) or die(mysql_error()); 
+		$row = mysql_fetch_array($query); 
+		
 
 
 		if(!empty($row['userName']) AND !empty($row['pass'])) 
@@ -30,11 +35,12 @@ session_start(); //starting the session for user profile page
 			echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY..."; 
 		} 
 
-	} 
+	}  
 } 
 
 if(isset($_POST['submit'])) 
 { 
+
 	SignIn(); 
 
 } 
