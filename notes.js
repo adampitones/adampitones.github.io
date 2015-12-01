973950ea50ccd5a509b0165dcc74c5ebacc9dc1f
Dey'Ana Wilson <Deyana033@users.noreply.github.com>
2015-12-01 11:54:20 -0600

    Update notes.js

--- notes.js
+++ notes.js
@@ -1,22 +1,198 @@
-tinymce.init({
-  selector: 'textarea',
-  height: 500,
-  theme: 'modern',
-  plugins: [
-    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-    'searchreplace wordcount visualblocks visualchars code fullscreen',
-    'insertdatetime media nonbreaking save table contextmenu directionality',
-    'emoticons template paste textcolor colorpicker textpattern imagetools'
-  ],
-  toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-  toolbar2: 'print preview media | forecolor backcolor emoticons',
-  image_advtab: true,
-  templates: [
-    { title: 'Test template 1', content: 'Test 1' },
-    { title: 'Test template 2', content: 'Test 2' }
-  ],
-  content_css: [
-    '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
-    '//www.tinymce.com/css/codepen.min.css'
-  ]
- });
+
+<!DOCTYPE html>
+<head>
+     <script type="text/javascript" src="notes.js"></script>
+</head>
+    <body>
+        <div id="header">
+             <h1>Lecture Notes</h1>
+        </div>
+
+        <div id="nav">
+                CSCE 436<a href="">CSCE 436</a><br>
+                CSCE 444<a href="">CSCE 444 </a><br>
+                Finc 409<a href="">FINC 409</a><br>
+                CSCE 464<a href="">CSCE 464</a><br>
+        </div>
+
+        <div id="section">
+            <h1>Notebook</h1>
+            <p>
+            	
+            	
+       
+            	
+               
+<script type="text/javascript">
+        bkLib.onDomLoaded(function() { nicEditors.allTextAreas() }); // convert all text areas to rich text editor on that page
+ 
+        bkLib.onDomLoaded(function() {
+             new nicEditor().panelInstance('area1');
+        }); // convert text area with id area1 to rich text editor.
+ 
+        bkLib.onDomLoaded(function() {
+             new nicEditor({fullPanel : true}).panelInstance('area2');
+        }); // convert text area with id area2 to rich text editor with full panel.
+</script>
+
+
+
+
+
+</p>
+</div>
+
+<div id="footer">
+classsssss
+</div>
+</body>
+</html> 
+
+
+
+
+<!--<HTML>
+
+<TITLE>Simple Notepad</TITLE>
+
+<HEAD>
+
+<script>
+
+function load_text(){
+
+document.getElementById("output").value = "Text....";
+
+}
+
+function bold_click(){
+
+document.getElementById("output").style.fontWeight = "bold";
+
+}
+
+function underline_click(){
+
+document.getElementById("output").style.textDecoration = "Underline";
+
+}
+
+ 
+
+function italic_click(){
+
+document.getElementById("output").style.fontStyle = "Italic";
+
+}
+
+ 
+
+function normal_click(){
+
+document.getElementById("output").style.fontStyle = "Normal";
+
+document.getElementById("output").style.textDecoration = "None";
+
+document.getElementById("output").style.fontWeight = "Normal";
+
+}
+
+</SCRIPT>
+
+</HEAD>
+
+<BODY onload="load_text()">
+
+ 
+
+<FORM NAME="controls" ID="controls">
+
+<SELECT style="WIDTH: 310px">
+
+<OPTION selected>Fonts</OPTION>
+
+<OPTION>Arial Black</OPTION>
+
+</SELECT>
+
+<INPUT id="button1" type="button" value="B" name="button1" style="FONT-WEIGHT: bold; WIDTH: 26px; HEIGHT: 24px" size="28" onclick="bold_click()">
+
+<INPUT type="button" value="I" style="WIDTH: 25px; FONT-STYLE: italic; HEIGHT: 24px" size="30" onclick="italic_click()">
+
+<INPUT id="button1" style="WIDTH: 26px; HEIGHT: 24px; TEXT-DECORATION: underline" type="button" size="8" value="U" name="button1" onclick="underline_click()">
+
+ 
+
+<INPUT id="button1" style="WIDTH: 26px; HEIGHT: 24px;" type="button" size="8" value="N" name="button1" onclick="normal_click()">
+
+ 
+
+<BR/><TEXTAREA style="WIDTH: 399px; HEIGHT: 182px" id="output" name="output" rows="9" wrap="virtual" cols=48></TEXTAREA><BR>
+
+<P align="center"><INPUT TYPE="reset" VALUE="Clear"></P>
+
+</FORM>
+
+
+</BODY>
+
+</HTML>
+
+
+
+
+
+
+
+
+
+-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style>
+#header {
+    background-color:black;
+    color:white;
+    text-align:center;
+    padding:5px;
+}
+#nav {
+    line-height:30px;
+    background-color:#eeeeee;
+    height:300px;
+    width:100px;
+    float:left;
+    padding:5px; 
+}
+#section {
+    width:350px;
+    float:left;
+    padding:10px; 
+}
+#footer {
+    background-color:black;
+    color:white;
+    clear:both;
+    text-align:center;
+    padding:5px; 
+}
+</style>
+
+
+
